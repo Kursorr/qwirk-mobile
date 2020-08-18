c5de7b2bf192e05d3a828a2c12ae6efcf93f00c5
Ravaniss <senechal.max06@gmail.com>
2020-08-18 05:27:44 +0200

    Footer

---
+++ app/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <Label text="Footer"/>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Footer extends Vue {
+}
+</script>
