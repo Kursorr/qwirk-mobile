d60a33e324e3ba1d2ed21eb7f96875944209c78a
Ravaniss <senechal.max06@gmail.com>
2020-08-18 08:05:15 +0200

    Footer - add logo on the footer

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,5 +1,10 @@
 <template>
-  <Label text="Footer"/>
+  <StackLayout>
+    <SVGImage src="~/assets/images/logo.svg"
+              class="selected"
+              height="25"
+              width="25"/>
+  </StackLayout>
 </template>
 
 <script lang="ts">
@@ -9,3 +14,9 @@
   export default class Footer extends Vue {
   }
 </script>
+
+<style lang="scss">
+  .selected {
+    opacity: 0.5;
+  }
+</style>
