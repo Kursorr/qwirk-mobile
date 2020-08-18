afd69cbb986f00d70f92301fd418aee85a47575a
Ravaniss <senechal.max06@gmail.com>
2020-08-19 00:11:16 +0200

    main - add nativescript-vue-navigator

--- app/main.js
+++ app/main.js
@@ -3,15 +3,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var tslib_1 = require("tslib");
 var nativescript_vue_1 = tslib_1.__importDefault(require("nativescript-vue"));
 var nativescript_vue_devtools_1 = tslib_1.__importDefault(require("nativescript-vue-devtools"));
+var nativescript_vue_navigator_1 = tslib_1.__importDefault(require("nativescript-vue-navigator"));
 var App_vue_1 = tslib_1.__importDefault(require("./views/App.vue"));
+var routes_1 = require("./routes");
 var store_1 = tslib_1.__importDefault(require("./store"));
+nativescript_vue_1.default.use(nativescript_vue_navigator_1.default, { routes: routes_1.routes });
 nativescript_vue_1.default.registerElement('SVGImage', function () { return require('@teammaestro/nativescript-svg').SVGImage; });
-if (TNS_ENV !== 'production') {
+if (TNS_ENV !== 'production')
     nativescript_vue_1.default.use(nativescript_vue_devtools_1.default);
-}
 // Prints Vue logs when --env.production is *NOT* set while building
 nativescript_vue_1.default.config.silent = (TNS_ENV === 'production');
 new nativescript_vue_1.default({
     store: store_1.default,
-    render: function (h) { return h('frame', [h(App_vue_1.default)]); }
+    render: function (h) { return h(App_vue_1.default); }
 }).$start();
--- app/main.ts
+++ app/main.ts
@@ -1,19 +1,24 @@
 import Vue from 'nativescript-vue'
 import VueDevtools from 'nativescript-vue-devtools'
 
+import Navigator from 'nativescript-vue-navigator'
+
 import App from './views/App.vue'
+
+import { routes } from './routes'
 import store from './store'
+
+Vue.use(Navigator, { routes })
 
 Vue.registerElement('SVGImage', () => require('@teammaestro/nativescript-svg').SVGImage)
 
-if (TNS_ENV !== 'production') {
+if (TNS_ENV !== 'production')
   Vue.use(VueDevtools)
-}
 
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production')
 
 new Vue({
   store,
-  render: h => h('frame', [h(App)])
+  render: h => h(App)
 }).$start()
