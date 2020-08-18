c91035cdde5a019fe905042ef25a39fea13414b6
Ravaniss <senechal.max06@gmail.com>
2020-08-19 00:13:08 +0200

    Profile - add profile page with a label

---
+++ app/views/Profile.vue
@@ -0,0 +1,24 @@
+<template>
+  <Page :actionBarHidden="true">
+    <StackLayout>
+      <Label text="Profile view ! :) OK"/>
+    </StackLayout>
+  </Page>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator'
+
+  @Component
+  export default class Profile extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+  StackLayout {
+    background-color: #1C1E22;
+  }
+
+  Label {
+    color: #FFF;
+  }
+</style>
