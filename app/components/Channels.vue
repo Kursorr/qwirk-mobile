a1041b71411dab1f296b3fe95e13a55915b78a65
Ravaniss <senechal.max06@gmail.com>
2020-08-18 05:28:06 +0200

    Channels component created with basic data

---
+++ app/components/Channels.vue
@@ -0,0 +1,31 @@
+<template>
+  <StackLayout borderRadius="10"
+               style="background: #333">
+    <label fontSize="16"
+           :textWrap="true"
+           :key="i"
+           v-for="(text,i) in loremTexts"
+           :text="text">
+    </label>
+  </StackLayout>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Channels extends Vue {
+  private loremTexts = [
+    '😇 You like this?',
+    '🤗 I saw the emojis from Discord',
+    '😎 I think they look cool',
+    '😁 I changed the colours as well'
+  ]
+}
+</script>
+
+<style lang="scss" scoped>
+  Label {
+    color: #FFF;
+  }
+</style>
