c9995ca351988f56237ed26bcb426fe9a2f39985
Ravaniss <senechal.max06@gmail.com>
2020-08-18 05:28:46 +0200

    Tchat created component with a label

---
+++ app/components/Tchat.vue
@@ -0,0 +1,20 @@
+<template>
+  <Label fontSize="18"
+         verticalAlignment="center"
+         textAlignment="center"
+         text="👈🏾 slide to the left or slide to the right 👉🏾" />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Tchat extends Vue {
+}
+</script>
+
+<style lang="scss">
+  Label {
+    color: #FFF;
+  }
+</style>
