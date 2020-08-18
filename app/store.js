d4d82b51cad6a3335c342c6c703bbcb4fe435ea7
Ravaniss <senechal.max06@gmail.com>
2020-08-18 05:24:34 +0200

    Store file

---
+++ app/store.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var tslib_1 = require("tslib");
+var vue_1 = tslib_1.__importDefault(require("vue"));
+var vuex_1 = tslib_1.__importDefault(require("vuex"));
+vue_1.default.use(vuex_1.default);
+exports.default = new vuex_1.default.Store({
+    state: {},
+    mutations: {},
+    actions: {}
+});
