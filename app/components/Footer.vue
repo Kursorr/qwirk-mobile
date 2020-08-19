d5ddeae4e2e79de532c77c47c4406d1903c271c1
Ravaniss <senechal.max06@gmail.com>
2020-08-19 22:11:45 +0200

    Footer - add navigate to profile with transition

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -15,7 +15,15 @@
   export default class Footer extends Vue {
     gotoProfile () {
       // @ts-ignore
-      this.$navigator.navigate('/profile', { clearHistory: true })
+      this.$navigator.navigate('profile', {
+        frame: 'navigator',
+        animated: true,
+        transition: {
+          name: 'slideLeft',
+          duration: 200,
+          curve: 'easeOut'
+        }
+      })
     }
   }
 </script>
