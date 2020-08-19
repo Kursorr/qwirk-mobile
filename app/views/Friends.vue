b3da60420498c1d4621768bfe56cd761ba66baef
Ravaniss <senechal.max06@gmail.com>
2020-08-19 04:17:43 +0200

    Friends - create view

---
+++ app/views/Friends.vue
@@ -0,0 +1,24 @@
+<template>
+  <Page :actionBarHidden="true">
+    <StackLayout>
+      <Label text="Friends view ! :) OK"/>
+    </StackLayout>
+  </Page>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator'
+
+  @Component
+  export default class Friends extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+  StackLayout {
+    background-color: #1C1E22;
+  }
+
+  Label {
+    color: #FFF;
+  }
+</style>
