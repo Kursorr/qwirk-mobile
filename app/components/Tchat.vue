499b3f53b240af5b6663937073cf74e10a1f9203
Ravaniss <senechal.max06@gmail.com>
2020-10-30 16:07:26 +0100

    re fix problem, remove stacklayout, and add @swipe action and now I can move the main panel (problem not fiexe btw)

--- app/components/Tchat.vue
+++ app/components/Tchat.vue
@@ -23,36 +23,31 @@
         </FlexboxLayout>
       </FlexboxLayout>
 
-      <StackLayout row="1">
-        <ScrollView>
-          <StackLayout marginTop="5">
-            <StackLayout margin="5 0" v-for="(discussion, index) in discussions">
-              <FlexboxLayout justifyContent="center">
-                <StackLayout height="40" width="80">
-                  <Image :src="discussion.avatar"
-                         height="40"
-                         width="40"
-                         borderRadius="25"
-                         stretch="aspectFill"/>
-                </StackLayout>
+      <ScrollView row="1" @swipe="onSwipe">
+        <StackLayout>
+          <StackLayout margin="5 0" v-for="(discussion, index) in discussions">
+            <FlexboxLayout justifyContent="center">
+              <StackLayout height="40" width="80">
+                <Image :src="discussion.avatar"
+                       height="40"
+                       width="40"
+                       borderRadius="25"
+                       stretch="aspectFill"/>
+              </StackLayout>
 
-                <StackLayout class="textInfos">
-                  <FlexboxLayout alignItems="center">
-                    <Label :text="discussion.pseudo" class="author"/>
-                    <Label :text="discussion.date" class="time"/>
-                  </FlexboxLayout>
-                  <Label :textWrap="true"
-                         class="text"
-                         :text="discussion.text"/>
-                </StackLayout>
-
-              </FlexboxLayout>
-            </StackLayout>
-
+              <StackLayout class="textInfos">
+                <FlexboxLayout alignItems="center">
+                  <Label :text="discussion.pseudo" class="author"/>
+                  <Label :text="discussion.date" class="time"/>
+                </FlexboxLayout>
+                <Label :textWrap="true"
+                       class="text"
+                       :text="discussion.text"/>
+              </StackLayout>
+            </FlexboxLayout>
           </StackLayout>
-
-        </ScrollView>
-      </StackLayout>
+        </StackLayout>
+      </ScrollView>
 
       <StackLayout row="2">
         <Label textWrap="true"
@@ -66,6 +61,7 @@
 
 <script lang="ts">
   import { Component, Vue } from 'vue-property-decorator'
+  import { SwipeDirection } from '@nativescript/core'
 
   @Component
   export default class Tchat extends Vue {
@@ -76,8 +72,118 @@
         pseudo: 'Kursorr',
         date: 'Aujourd\'hui à 20:58',
         text: 'A super long text, long that should wrap correctly at the bottom... Let\'s prey that happen correctly'
+      },
+      {
+        id: 0,
+        avatar: '~/assets/images/avatar.jpeg',
+        pseudo: 'Kursorr',
+        date: 'Aujourd\'hui à 20:58',
+        text: 'A super long text, long that should wrap correctly at the bottom... Let\'s prey that happen correctly'
+      },
+      {
+        id: 0,
+        avatar: '~/assets/images/avatar.jpeg',
+        pseudo: 'Kursorr',
+        date: 'Aujourd\'hui à 20:58',
+        text: 'A super long text, long that should wrap correctly at the bottom... Let\'s prey that happen correctly'
+      },
+      {
+        id: 0,
+        avatar: '~/assets/images/avatar.jpeg',
+        pseudo: 'Kursorr',
+        date: 'Aujourd\'hui à 20:58',
+        text: 'A super long text, long that should wrap correctly at the bottom... Let\'s prey that happen correctly'
+      },
+      {
+        id: 0,
+        avatar: '~/assets/images/avatar.jpeg',
+        pseudo: 'Kursorr',
+        date: 'Aujourd\'hui à 20:58',
+        text: 'A super long text, long that should wrap correctly at the bottom... Let\'s prey that happen correctly'
+      },
+      {
+        id: 0,
+        avatar: '~/assets/images/avatar.jpeg',
+        pseudo: 'Kursorr',
+        date: 'Aujourd\'hui à 20:58',
+        text: 'A super long text, long that should wrap correctly at the bottom... Let\'s prey that happen correctly'
+      },
+      {
+        id: 0,
+        avatar: '~/assets/images/avatar.jpeg',
+        pseudo: 'Kursorr',
+        date: 'Aujourd\'hui à 20:58',
+        text: 'A super long text, long that should wrap correctly at the bottom... Let\'s prey that happen correctly'
+      },
+      {
+        id: 0,
+        avatar: '~/assets/images/avatar.jpeg',
+        pseudo: 'Kursorr',
+        date: 'Aujourd\'hui à 20:58',
+        text: 'A super long text, long that should wrap correctly at the bottom... Let\'s prey that happen correctly'
+      },
+      {
+        id: 0,
+        avatar: '~/assets/images/avatar.jpeg',
+        pseudo: 'Kursorr',
+        date: 'Aujourd\'hui à 20:58',
+        text: 'A super long text, long that should wrap correctly at the bottom... Let\'s prey that happen correctly'
+      },
+      {
+        id: 0,
+        avatar: '~/assets/images/avatar.jpeg',
+        pseudo: 'Kursorr',
+        date: 'Aujourd\'hui à 20:58',
+        text: 'A super long text, long that should wrap correctly at the bottom... Let\'s prey that happen correctly'
+      },
+      {
+        id: 0,
+        avatar: '~/assets/images/avatar.jpeg',
+        pseudo: 'Kursorr',
+        date: 'Aujourd\'hui à 20:58',
+        text: 'A super long text, long that should wrap correctly at the bottom... Let\'s prey that happen correctly'
+      },
+      {
+        id: 0,
+        avatar: '~/assets/images/avatar.jpeg',
+        pseudo: 'Kursorr',
+        date: 'Aujourd\'hui à 20:58',
+        text: 'A super long text, long that should wrap correctly at the bottom... Let\'s prey that happen correctly'
+      },
+      {
+        id: 0,
+        avatar: '~/assets/images/avatar.jpeg',
+        pseudo: 'Kursorr',
+        date: 'Aujourd\'hui à 20:58',
+        text: 'A super long text, long that should wrap correctly at the bottom... Let\'s prey that happen correctly'
+      },
+      {
+        id: 0,
+        avatar: '~/assets/images/avatar.jpeg',
+        pseudo: 'Kursorr',
+        date: 'Aujourd\'hui à 20:58',
+        text: 'A super long text, long that should wrap correctly at the bottom... Let\'s prey that happen correctly'
+      },
+      {
+        id: 0,
+        avatar: '~/assets/images/avatar.jpeg',
+        pseudo: 'Kursorr',
+        date: 'Aujourd\'hui à 20:58',
+        text: 'A super long text, long that should wrap correctly at the bottom... Let\'s prey that happen correctly'
       }
     ]
+
+    onSwipe (args) {
+      let direction =
+          args.direction == SwipeDirection.down
+              ? "down"
+              : args.direction == SwipeDirection.up
+              ? "up"
+              : args.direction == SwipeDirection.left
+                  ? "left"
+                  : "right";
+      console.log(`You performed a ${direction} swipe`)
+    }
   }
 </script>
 
