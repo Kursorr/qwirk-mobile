680514e04c481e7d8b9fa9ed59d9f6d4c701e330
Ravaniss <senechal.max06@gmail.com>
2020-11-03 15:33:52 +0100

    main - add the possibility to call the header everywhere on the app

--- app/main.js
+++ app/main.js
@@ -7,10 +7,12 @@ var nativescript_vue_navigator_1 = tslib_1.__importDefault(require("nativescript
 var orientation = tslib_1.__importStar(require("nativescript-screen-orientation"));
 var nativescript_vue_devtools_1 = tslib_1.__importDefault(require("nativescript-vue-devtools"));
 var App_vue_1 = tslib_1.__importDefault(require("./views/App.vue"));
+var Header_vue_1 = tslib_1.__importDefault(require("./components/Layout/Header.vue"));
 var routes_1 = require("./routes/");
 var store_1 = tslib_1.__importDefault(require("./store/"));
 require('./app.scss');
 var icons = require('./assets/icons/icons.json');
+nativescript_vue_1.default.component('Header', Header_vue_1.default);
 nativescript_vue_1.default.use(vue_1.default);
 nativescript_vue_1.default.use(nativescript_vue_navigator_1.default, { routes: routes_1.routes });
 if (TNS_ENV !== 'production')
--- app/main.ts
+++ app/main.ts
@@ -6,12 +6,15 @@ import * as orientation from 'nativescript-screen-orientation'
 import VueDevtools from 'nativescript-vue-devtools'
 
 import App from './views/App.vue'
+import Header from './components/Layout/Header.vue'
 
 import { routes } from './routes/'
 import store from './store/'
 
 require('./app.scss')
 const icons = require('./assets/icons/icons.json')
+
+Vue.component('Header', Header)
 
 Vue.use(Copilot)
 Vue.use(Navigator, { routes })
