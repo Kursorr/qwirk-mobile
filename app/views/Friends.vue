aa848833e4daa09305aceafd54bfe845073a3777
Ravaniss <senechal.max06@gmail.com>
2020-11-03 15:34:35 +0100

    Friends - Add Header component

--- app/views/Friends.vue
+++ app/views/Friends.vue
@@ -3,24 +3,7 @@
     <GridLayout id="friends"
                 columns="*"
                 rows="auto, *, auto">
-      <FlexboxLayout class="headerSection" row="0">
-        <FlexboxLayout class="header">
-          <FlexboxLayout alignItems="center">
-            <Label :text="String.fromCharCode($icons.menu)"
-                   class="ico menu"/>
-            <Label :text="String.fromCharCode($icons.hashtag)"
-                   class="ico hashtag"/>
-            <Label text="general"
-                   class="title"/>
-          </FlexboxLayout>
-          <FlexboxLayout>
-            <Label :text="String.fromCharCode($icons.search)"
-                   class="ico search"/>
-            <Label :text="String.fromCharCode($icons.users)"
-                   class="ico users"/>
-          </FlexboxLayout>
-        </FlexboxLayout>
-      </FlexboxLayout>
+      <Header />
     </GridLayout>
   </Page>
 </template>
