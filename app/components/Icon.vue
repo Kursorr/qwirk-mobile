2e84b3fcb4e09b1b8bde80a69681c508c870c669
Ravaniss <senechal.max06@gmail.com>
2020-11-03 20:40:52 +0100

    Icon component

---
+++ app/components/Icon.vue
@@ -0,0 +1,13 @@
+<template>
+  <Label :text="String.fromCharCode(icon)"
+         class="ico users"/>
+</template>
+
+<script lang="ts">
+  import { Vue, Component, Prop } from 'vue-property-decorator'
+
+  @Component
+  export default class Icon extends Vue {
+    @Prop() icon
+  }
+</script>
