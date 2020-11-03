a16ddc8a81076e30ff548820005cfe93eaebf9a6
Ravaniss <senechal.max06@gmail.com>
2020-11-03 15:34:13 +0100

    Header component

---
+++ app/components/Layout/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <FlexboxLayout class="headerSection" row="0">
+    <FlexboxLayout class="header">
+      <FlexboxLayout alignItems="center">
+        <Label :text="String.fromCharCode($icons.menu)"
+               class="ico menu"/>
+        <Label :text="String.fromCharCode($icons.hashtag)"
+               class="ico hashtag"/>
+        <Label text="general"
+               class="title"/>
+      </FlexboxLayout>
+      <FlexboxLayout>
+        <Label :text="String.fromCharCode($icons.search)"
+               class="ico search"/>
+        <Label :text="String.fromCharCode($icons.users)"
+               class="ico users"/>
+      </FlexboxLayout>
+    </FlexboxLayout>
+  </FlexboxLayout>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
