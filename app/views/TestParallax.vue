57cb9bb99088e3be1a2bedd8b00846190bb76249
Ravaniss <senechal.max06@gmail.com>
2021-01-02 01:40:22 +0100

    remove unecessary

--- app/views/TestParallax.vue
+++ app/views/TestParallax.vue
@@ -15,105 +15,105 @@
 </template>
 
 <script lang="ts">
-import {
-  AndroidSensorListener,
-  AndroidSensors,
-  SensorDelay,
-} from 'nativescript-android-sensors'
+  import {
+    AndroidSensorListener,
+    AndroidSensors,
+    SensorDelay,
+  } from 'nativescript-android-sensors'
 
-import { Component, Vue } from 'vue-property-decorator'
+  import { Component, Vue } from 'vue-property-decorator'
 
-function getAvg (items) {
-  const total = items.reduce((acc, c) => acc + c, 0)
-  return total / items.length
-}
-
-@Component
-export default class TestParallax extends Vue {
-  public message: string
-  private androidSensors: AndroidSensors
-  public sensorMaxFifoEventCount: string = ''
-  public sensorData: string = ''
-  public xarr: Array<number> = [0, 0, 0, 0, 0]
-  public yarr: Array<number> = [0, 0, 0, 0, 0]
-
-  public get tx(): number {
-    return getAvg(this.xarr)
+  function getAvg (items) {
+    const total = items.reduce((acc, c) => acc + c, 0)
+    return total / items.length
   }
 
-  public get ty(): number {
-    return getAvg(this.yarr)
+  @Component
+  export default class TestParallax extends Vue {
+    public message: string
+    private androidSensors: AndroidSensors
+    public sensorMaxFifoEventCount: string = ''
+    public sensorData: string = ''
+    public xarr: Array<number> = [0, 0, 0, 0, 0]
+    public yarr: Array<number> = [0, 0, 0, 0, 0]
+
+    public get tx(): number {
+      return getAvg(this.xarr)
+    }
+
+    public get ty(): number {
+      return getAvg(this.yarr)
+    }
+
+    constructor() {
+      super()
+
+      this.androidSensors = new AndroidSensors()
+
+      const listener = new AndroidSensorListener({
+        onSensorChanged: (result) => {
+          // console.log('SensorChangedEvent', result)
+          // result is being returned as a string currently
+          const parsedData = JSON.parse(result)
+          const rawSensorData = parsedData.data
+          this.sensorData = rawSensorData.x
+          // const sensor = parsedData.sensor
+          // const time = parsedData.time
+
+          // @ts-ignore
+          this.$refs.perso.nativeView.animate({
+            translate: {
+              x: this.tx,
+              y: this.ty
+            }
+          })
+
+          // @ts-ignore
+          this.$refs.back.nativeView.animate({
+            translate: {
+              x: 0,
+              y: this.ty * 0.4
+            }
+          })
+
+          this.xarr.shift()
+          this.xarr.push(Math.round(rawSensorData.x * 100))
+
+          this.yarr.shift()
+          this.yarr.push(Math.round(rawSensorData.y * 100))
+        },
+        onAccuracyChanged: (sensor, accuracy) => {
+          // console.log('Accuracy Changed', sensor, accuracy)
+        },
+      })
+
+      this.androidSensors.setListener(listener)
+    }
+
+    public startLinearAcceleration() {
+      // starting the acceleration sensor
+      const acceleration = this.androidSensors.startSensor(
+          // @ts-ignore
+          android.hardware.Sensor.TYPE_LINEAR_ACCELERATION,
+          SensorDelay.UI, 0)
+
+      // checking if it supports FIFO
+      const x = (acceleration as any).getFifoMaxEventCount()
+      this.sensorMaxFifoEventCount = `Max Fifo Event Count: ${x}`
+
+      // after 8 seconds we are stopping the acceleration sensor
+      /*setTimeout(() => {
+        this.androidSensors.stopSensor(acceleration)
+        this.sensorData = 'Sensor has stopped!'
+      }, 20000)*/
+    }
+
+    private mounted () {
+      setTimeout(() => {
+        this.startLinearAcceleration()
+      }, 1000)
+    }
   }
-
-  constructor() {
-    super()
-
-    this.androidSensors = new AndroidSensors()
-
-    const listener = new AndroidSensorListener({
-      onSensorChanged: (result) => {
-        // console.log('SensorChangedEvent', result)
-        // result is being returned as a string currently
-        const parsedData = JSON.parse(result)
-        const rawSensorData = parsedData.data
-        this.sensorData = rawSensorData.x
-        // const sensor = parsedData.sensor
-        // const time = parsedData.time
-
-        // @ts-ignore
-        this.$refs.perso.nativeView.animate({
-          translate: {
-            x: this.tx,
-            y: this.ty
-          }
-        })
-
-        // @ts-ignore
-        this.$refs.back.nativeView.animate({
-          translate: {
-            x: 0,
-            y: this.ty * 0.4
-          }
-        })
-
-        this.xarr.shift()
-        this.xarr.push(Math.round(rawSensorData.x * 100))
-
-        this.yarr.shift()
-        this.yarr.push(Math.round(rawSensorData.y * 100))
-      },
-      onAccuracyChanged: (sensor, accuracy) => {
-        // console.log('Accuracy Changed', sensor, accuracy)
-      },
-    })
-
-    this.androidSensors.setListener(listener)
-  }
-
-  public startLinearAcceleration() {
-    // starting the acceleration sensor
-    const acceleration = this.androidSensors.startSensor(
-        // @ts-ignore
-        android.hardware.Sensor.TYPE_LINEAR_ACCELERATION,
-        SensorDelay.UI, 0)
-
-    // checking if it supports FIFO
-    const x = (acceleration as any).getFifoMaxEventCount()
-    this.sensorMaxFifoEventCount = `Max Fifo Event Count: ${x}`
-
-    // after 8 seconds we are stopping the acceleration sensor
-    /*setTimeout(() => {
-      this.androidSensors.stopSensor(acceleration)
-      this.sensorData = 'Sensor has stopped!'
-    }, 20000)*/
-  }
-
-  private mounted () {
-    setTimeout(() => {
-      this.startLinearAcceleration()
-    }, 1000)
-  }
-}
 </script>
 
 <style>
